6fdfff7717d8702843cfccc5f1dd8c2eca45ff7b
beucismis <beucismis@tutamail.com>
2021-04-26 22:04:14 +0300

    Added on_invite listener

--- rtfm-bot/cogs/event.py
+++ rtfm-bot/cogs/event.py
@@ -10,6 +10,8 @@ def setup(bot):
 class Event(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.invite_code = "9P5HvT9nPD"
+        self.invite_address = "https://discord.com/invite/{}".format(self.invite_code)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -33,3 +35,11 @@ class Event(commands.Cog):
 
         if author.bot:
             return
+
+    @commands.Cog.listener(name="on_message")
+    async def on_invite(self, message):
+        content = message.content.lower()
+
+        if content.startswith("davet") or content.startswith("invite"):
+            invite = await self.bot.fetch_invite(self.invite_address)
+            await message.reply(invite.url)

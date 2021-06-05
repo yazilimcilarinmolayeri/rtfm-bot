b8c9271174e71711da28b5ce94864035e241a372
Recep Emre Ada <73690406+EmreAda@users.noreply.github.com>
2021-06-05 16:31:14 +0300

    little fix

--- rtfm-bot/cogs/event.py
+++ rtfm-bot/cogs/event.py
@@ -32,13 +32,16 @@ class Event(commands.Cog):
     @commands.Cog.listener(name="on_message")
     async def on_chat(self, message):
         author = message.author
-
         if author.bot:
             return
 
     @commands.Cog.listener(name="on_message")
     async def on_invite(self, message):
         content = message.content.lower()
+        author = message.author
+        if author.bot:
+            return
+
 
         if content.startswith("davet") or content.startswith("invite"):
             invite = await self.bot.fetch_invite(self.invite_address)
